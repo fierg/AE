135b2d1eeac0864c2c87c7e43c1c9d0be6e038d1
Sven Fiergolla <sven.fiergolla@gmail.com>
2023-07-18 19:56:48 +0200

    Added main for merge example

--- src/Merge/main.cpp
+++ src/Merge/main.cpp
@@ -6,12 +6,18 @@
 #include <iostream>
 
 int main() {
-    std::string str1 = "level";
-    std::string str2 = "hello";
-    std::string out = "";
+    int a[] = {1, 3, 5, 7};
+    int b[] = {2, 4, 6, 8};
+    int c[] = {10, 12, 14, 16};
+    int d[] = {9, 11, 13, 15};
+    int e[15];
 
-    merge(str1.begin(),str1.end(),str2.begin(),str2.end(),out.begin());
-    std::cout << "merged String: " << out << std::endl;
+    merge(std::begin(a), std::end(a), std::begin(b), std::end(b), std::begin(e));
+    merge(std::begin(c), std::end(c), std::begin(d), std::end(d), std::begin(e) + 8);
 
+    std::cout << "Merged sequence: ";
+    for (const auto& element : e) {
+        std::cout << element << " ";
+    }
     return 0;
 }
